b569f6d2edffdd4ef6b1f060e4976a2261841e1a
= <gjhunt@umich.edu>
2016-12-03 22:50:26 -0500

    Edits

--- prelim.R
+++ prelim.R
@@ -28,39 +28,41 @@ source('./latin_R/chap4-fig3.R')
 
 ## ----lit-child, child='Analysis/Analysis.Rnw'----------------------------
 
-## ----plot1,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,fig.height=5,fig.width=7----
-source('./R/Analysis_script.R',chdir=TRUE)
-sel = sel=c(TRUE,FALSE,TRUE,FALSE,FALSE,FALSE)
-aplot(19830,'diag',sel)
-
-## ----plot2,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,fig.height=5,fig.width=7----
-source('./R/Analysis_script.R',chdir=TRUE)
-sel = sel=c(TRUE,FALSE,TRUE,FALSE,FALSE,FALSE)
-aplot(19830,'err_boxplot',sel)
-
 ## ----table1,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,results='asis'----
 library('xtable')
 source('./R/Analysis_script.R',chdir=TRUE)
 sel = sel=c(TRUE,FALSE,TRUE,FALSE,FALSE,FALSE)
 t = aplot(19830,'summ',sel)
-print(xtable(t),include.rownames=FALSE)
+print(xtable(t,caption="Quantiles of the absolute error of predicted mixing proportions.",
+             label="tab:rat1"
+             ),include.rownames=FALSE)
 
-## ----plot3,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,fig.height=5,fig.width=7----
+## ----plot2,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,fig.height=4,fig.width=7----
 source('./R/Analysis_script.R',chdir=TRUE)
 sel = sel=c(TRUE,FALSE,TRUE,FALSE,FALSE,FALSE)
-aplot(11058,'diag',sel)
+aplot(19830,'err_boxplot',sel)
 
-## ----plot4,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,fig.height=5,fig.width=7----
+## ----plot1,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,fig.height=5,fig.width=7----
 source('./R/Analysis_script.R',chdir=TRUE)
 sel = sel=c(TRUE,FALSE,TRUE,FALSE,FALSE,FALSE)
-aplot(11058,'err_boxplot',sel)
+aplot(19830,'diag',sel)
 
 ## ----table2,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,results='asis'----
 library('xtable')
 source('./R/Analysis_script.R',chdir=TRUE)
 sel = sel=c(TRUE,FALSE,TRUE,FALSE,FALSE,FALSE)
 t = aplot(11058,'summ',sel)
-print(xtable(t),include.rownames=FALSE)
+print(xtable(t,label="tab:blood",caption="Quantiles of the absolute error of predicted mixing proportions."),include.rownames=FALSE)
+
+## ----plot4,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,fig.height=4,fig.width=7----
+source('./R/Analysis_script.R',chdir=TRUE)
+sel = sel=c(TRUE,FALSE,TRUE,FALSE,FALSE,FALSE)
+aplot(11058,'err_boxplot',sel)
+
+## ----plot3,cache=FALSE,warning=FALSE,echo=FALSE,error=FALSE,message=FALSE,fig.height=5,fig.width=7----
+source('./R/Analysis_script.R',chdir=TRUE)
+sel = sel=c(TRUE,FALSE,TRUE,FALSE,FALSE,FALSE)
+aplot(11058,'diag',sel)
 
 
 ## ----lit-child, child='Conclusion/Conclusion.Rnw'------------------------
